4beac7fd3d765233561ce3b80a0fa4d3cb2d9cfb
Roman <kenix1337@yandex.ru>
2019-11-11 03:26:29 +0400

    minor changes to the logic and adding styles

--- script/script.js
+++ script/script.js
@@ -5,6 +5,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const taskList = document.querySelector(".list");
     const addTaskButton = document.querySelector(".task-input__add");
     const clearTaskButton = document.querySelector(".task-input__clear");
+    const doneAllTaskButton = document.querySelector(".task-input__done");
     const collectionOfTasks = [];
 
     /*Functionality*/
@@ -26,12 +27,24 @@ document.addEventListener("DOMContentLoaded", () => {
 
         collectionOfTasks.splice(indexTask, 1);
     };
-    const makeDoneTask = task => {
-        collectionOfTasks.forEach(item => {
+    const doneTask = (indexTask) => {
+        collectionOfTasks[indexTask].done= true;
+    };
+    const unfulfilledTask = (indexTask) => {
+        collectionOfTasks[indexTask].done= false;
+    };
+    const changeStateTask = task => {  
+        collectionOfTasks.forEach((item, index) => {
             if (item.name === task) {
-                item.done = true;
+                item.done ? unfulfilledTask(index) : doneTask(index);
             }
         });
+    };
+    const makeDoneAllTasks = () => {
+        collectionOfTasks.forEach((item, index) => {
+            doneTask(index);
+        });
+        renderTaskList(collectionOfTasks);
     };
     const hasTask = taskName => {
         const findedTask = collectionOfTasks.findIndex(item => (item.name === taskName));
@@ -45,12 +58,38 @@ document.addEventListener("DOMContentLoaded", () => {
     const checkEmptyField = ({value}) => {
         return (value === "");
     };
+    const renameTask = (taskName, newName) => {
+        collectionOfTasks.forEach(item => {
+            if (item.name === taskName) {
+                item.name = newName;
+            }
+        });
+    }
+    const editTaskName = taskRow => {
+        const hasEditField = taskRow.querySelector(".editor");
+
+        if (hasEditField) return;
+
+        const editField = document.createElement("input");
+        const taskName = taskRow.textContent.trim();
+
+        editField.type = "text";
+        editField.value = taskName;
+        editField.classList.add("editor");
+        taskRow.innerHTML = "";
+        taskRow.append(editField);
+
+        editField.addEventListener("blur", ({target}) => {
+            renameTask(taskName, target.value);
+            renderTaskList(collectionOfTasks);
+        });
+    }
     const renderTaskRow = task => {
         const {name} = task;
         const li = document.createElement("li");
         const innerLi = `
+            <label class="label-checkbox"><input type="checkbox" class="list-checkbox" id="list-checkbox"></label>
             <span class="task-text">${name}</span>
-            <input type="checkbox" class="list-checkbox">
             <div class="list__remove-button"></div>
         `;
         if (task.done) {
@@ -95,20 +134,26 @@ document.addEventListener("DOMContentLoaded", () => {
         clearTaskList(collectionOfTasks);
     });
 
+    doneAllTaskButton.addEventListener("click", () => {
+        makeDoneAllTasks();
+    });
+
     taskList.addEventListener("click", ({target}) => {
         const taskRow = target.closest(".list-item");
         const taskName = taskRow.querySelector(".task-text").textContent.trim();
 
         if (target.classList.contains("list-checkbox")) {
             taskRow.classList.toggle("done-task");
-            makeDoneTask(taskName);
+            changeStateTask(taskName);
         } else if (target.classList.contains("list__remove-button")) {
-            taskRow.remove();
             removeTask(taskName);
             renderTaskList(collectionOfTasks);
+        } else if (target.classList.contains("task-text")) {
+            editTaskName(target);
         } else {
-            return false;
+            return;
         }
+
     });  
 
 });
